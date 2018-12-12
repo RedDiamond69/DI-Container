f6c133282a4bfb790b0880d677030d6b474b5a6a
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-12 03:44:58 +0300

    IDependencyConfig implemented. Created ImplementationInfo class.

--- DependencyInjectionContainer/DependencyConfig/IDependencyConfig.cs
+++ DependencyInjectionContainer/DependencyConfig/IDependencyConfig.cs
@@ -8,5 +8,12 @@ namespace DependencyInjectionContainer.DependencyConfig
 {
     public interface IDependencyConfig
     {
+        IEnumerable<ImplementationInfo.ImplementationInfo> GetImplementations(Type type);
+
+        void Register(Type dependency, Type implementation, bool isSingleton = false, string name = null);
+
+        void Register<TDependency, TImplementation>(bool isSingleton = false, string name = null)
+            where TDependency : class
+            where TImplementation : TDependency;
     }
 }
