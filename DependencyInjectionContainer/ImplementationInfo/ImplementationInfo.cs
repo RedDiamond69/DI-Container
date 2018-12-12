d655ba8918b780dd185e4c9dc659edba798cf277
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-12 03:49:52 +0300

    ImplementationInfo class implemented.

--- DependencyInjectionContainer/ImplementationInfo/ImplementationInfo.cs
+++ DependencyInjectionContainer/ImplementationInfo/ImplementationInfo.cs
@@ -8,5 +8,17 @@ namespace DependencyInjectionContainer.ImplementationInfo
 {
     public class ImplementationInfo
     {
+        public Type ImplementationType { get; }
+        public bool IsSingleton { get; }
+        public object SingletonInstance { get; set; }
+        public string Name { get; }
+
+        public ImplementationInfo(Type type, bool isSingleton, string name)
+        {
+            ImplementationType = type;
+            Name = name;
+            SingletonInstance = null;
+            IsSingleton = isSingleton;
+        }
     }
 }
