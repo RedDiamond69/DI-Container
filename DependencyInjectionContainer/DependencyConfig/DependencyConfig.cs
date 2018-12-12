80f23a3759fd50f3c1c66446911a9cc2d483fbe0
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-12 03:52:13 +0300

    Created DependencyConfig class and added empty constructor, IDependencyConfig inherited and added empty implementation for him.

---
+++ DependencyInjectionContainer/DependencyConfig/DependencyConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyInjectionContainer.ImplementationInfo;
+
+namespace DependencyInjectionContainer.DependencyConfig
+{
+    public class DependencyConfig : IDependencyConfig
+    {
+        public DependencyConfig()
+        {
+        }
+
+        public IEnumerable<ImplementationInfo.ImplementationInfo> GetImplementations(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type dependency, Type implementation, bool isSingleton = false, string name = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TDependency, TImplementation>(bool isSingleton = false, string name = null)
+            where TDependency : class
+            where TImplementation : TDependency
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
