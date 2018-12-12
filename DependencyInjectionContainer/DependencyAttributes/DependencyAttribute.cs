0843bb873a17a16b51463906ea9cc996ab1caaf3
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-12 04:57:24 +0300

    DependencyAttribute class implemented.

--- DependencyInjectionContainer/DependencyAttributes/DependencyAttribute.cs
+++ DependencyInjectionContainer/DependencyAttributes/DependencyAttribute.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DependencyInjectionContainer.DependencyAttributes
 {
-    public class DependencyAttribute
+    public class DependencyAttribute : Attribute
     {
+        public string Name { get; protected set; }
+
+        public DependencyAttribute(string name)
+        {
+            Name = name;
+        }
     }
 }
