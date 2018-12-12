17f50804c37cec0d67a1f4e450b55022ba7c7ead
Alexey Kosyuk <reddiamond821@gmail.com>
2018-12-12 04:30:42 +0300

    ExtensionMethod implemented.

--- DependencyInjectionContainer/ExtensionMethodForTypeClass/ExtensionMethod.cs
+++ DependencyInjectionContainer/ExtensionMethodForTypeClass/ExtensionMethod.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace DependencyInjectionContainer.ExtensionMethodForTypeClass
 {
-    public class ExtensionMethod
+    public static class ExtensionMethod
     {
+        public static bool IsAssignableFromGeneric(this Type type, Type t)
+        {
+            if (!type.IsGenericTypeDefinition || !t.IsGenericTypeDefinition)
+                throw new ArgumentException("Specified types should be generic");
+            Type comparedType, baseType;
+            Queue<Type> baseTypes = new Queue<Type>();
+            baseTypes.Enqueue(t);
+            bool result = false;
+            do
+            {
+                comparedType = baseTypes.Dequeue();
+                baseType = comparedType.BaseType;
+                if ((baseType != null) && (baseType.IsGenericType || baseType.IsGenericTypeDefinition))
+                    baseTypes.Enqueue(baseType.GetGenericTypeDefinition());
+                foreach (Type baseInterface in 
+                    comparedType.GetInterfaces()
+                    .Where((intf) => intf.IsGenericType || intf.IsGenericTypeDefinition))
+                    baseTypes.Enqueue(baseInterface.GetGenericTypeDefinition());
+                result = comparedType == type;
+            } while (!result && (baseTypes.Count > 0));
+            return result;
+        }
     }
 }
